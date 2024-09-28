f5ab5d514ba7c86e173cfab02a8f558027af0492
Krishnatejaswi S <shentharkrishnatejaswi@gmail.com>
2024-09-28 20:45:34 +0530

    Completed the Package

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
-
+# make a function to read the Readme and use it a long description but in a single string
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 setup(
     name='pageweaver',
-    version='0.1.0',
+    version='1.0.0',
+    author='Krishnatejaswi S, Sridhar D Kedlaya',
+    author_email='shentharkrishnatejaswi@gmail.com, sridhardked@gmail.com',
+    description='A web crawler to fetch web novel chapters and generate a PDF.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/KTS-o7/pageweaver',
+    keywords=['web novel', 'crawler', 'PDF generation', 'web scraping'],
     packages=find_packages(),
     install_requires=[
         'requests',
@@ -15,12 +24,6 @@ setup(
             'pageweaver=pageweaver.novel_crawler_service:main',
         ],
     },
-    author='Krishnatejaswi S, Sridhar D Kedlaya',
-    author_email='shentharkrishnatejaswi@gmail.com, sridhardkedlaya.cs21@rvce.edu.in',
-    description='A web crawler to fetch web novel chapters and generate a PDF.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/KTS-o7/pageweaver.git',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
