a7eed731933708e5743616ddb87b291c83f36c15
Krishnatejaswi S <shentharkrishnatejaswi@gmail.com>
2024-09-28 19:53:11 +0530

    Complete Package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pageweaver',
-    version='1.0.0',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
         'requests',
